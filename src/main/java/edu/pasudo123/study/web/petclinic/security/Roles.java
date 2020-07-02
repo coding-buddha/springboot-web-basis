cf97011e79c3865ba0fcdac2eb2cbdc8f1e2fef0
PARK SUNG DONG <oraedoa@gmail.com>
2020-07-02 17:09:38 +0900

    [add] security

---
+++ src/main/java/edu/pasudo123/study/web/petclinic/security/Roles.java
@@ -0,0 +1,9 @@
+package edu.pasudo123.study.web.petclinic.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Roles {
+    public final String OWNER_ADMIN = "ROLE_OWNER_ADMIN";
+    public final String VET_ADMIN = "ROLE_VET_ADMIN";
+}
